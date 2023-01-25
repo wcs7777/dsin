4ae7ca7464a0b7a8f0c595a54df15296934d9818
Willian Carlos <wcs777git@outlook.com>
2023-01-25 04:43:44 -0300

    não tinha feito por falta de tempo

--- 11.sql
+++ 11.sql
@@ -1,3 +1,4 @@
+/* A */
 SELECT
 	c.cdclinome AS 'Cliente',
 	a.lcadatahoraatend AS 'Data e Hora do Atendimento',
@@ -10,3 +11,30 @@ INNER JOIN
 	cdfuncionario AS f ON f.cdfcodigo = a.lcafuncionario
 ORDER BY
 	c.cdclinome;
+
+/* B (Não tinha feito por falta de tempo) */
+SELECT
+	c.cdclicodigo AS 'Código do Cliente',
+	c.cdclinome AS 'Nome do Cliente'
+FROM
+	cdclicodigo AS c
+INNER JOIN
+	lcatendimento AS a ON c.cdclicodigo = a.lcacliente
+GROUP BY
+	c.cdclicodigo,
+	c.cdclinome
+HAVING
+	COUNT(*) > 3;
+
+
+/* C (Não tinha feito por falta de tempo) */
+SELECT
+	c.cdclinome AS 'Cliente',
+	a.lcadatahoraatend AS 'Data e Hora do Atendimento'
+FROM
+	cdclicodigo AS c
+INNER JOIN
+	lcatendimento AS a ON c.cdclicodigo = a.lcacliente
+WHERE
+	YEAR(a.lcadatahoraatend) = 2011 AND
+	(MONTH(a.lcadatahoraatend) < 3 OR MONTH(a.lcadatahoraatend) > 6);
