398977bc9a783d9b9d3c0d174631ea4a6aea213b
Willian Carlos <wcs777git@outlook.com>
2023-01-25 04:05:28 -0300

    finalmente consegui fazer o quadrado de caracol

---
+++ 08.js
@@ -0,0 +1,72 @@
+for (let sides = 1; sides < 10; ++sides) {
+	console.log(`Sides: ${sides}`);
+	console.log(squareToString(snailSquare(sides), sides));
+}
+
+function snailSquare(side) {
+	let index = 0;
+	let value = 2;
+	let limit = side;
+	const lastValue = side ** 2 * 2;
+	const square = [];
+	const shouldContinue = () => value <= lastValue;
+	while (shouldContinue()) {
+		repeatFn(limit, shouldContinue, () => {
+			square[index] = value;
+			index++;
+			value += 2;
+		});
+		if (!shouldContinue()) {
+			break;
+		}
+		--limit;
+		--index;
+		repeatFn(limit, shouldContinue, () => {
+			index += side;
+			square[index] = value;
+			value += 2;
+		});
+		if (!shouldContinue()) {
+			break;
+		}
+		repeatFn(limit, shouldContinue, () => {
+			--index;
+			square[index] = value;
+			value += 2;
+		});
+		if (!shouldContinue()) {
+			break;
+		}
+		--limit;
+		repeatFn(limit, shouldContinue, () => {
+			index -= side;
+			square[index] = value;
+			value += 2;
+		});
+		if (!shouldContinue()) {
+			break;
+		}
+		++index;
+	}
+	return square;
+}
+
+function repeatFn(quantity, shouldContinue, fn) {
+	for (let i = 0; shouldContinue(i) && i < quantity; ++i) {
+		if (!shouldContinue(i)) {
+			console.log("repeatFn".repeat(20));
+		}
+		fn(i);
+	}
+}
+
+function squareToString(square, side) {
+	const out = [];
+	for (let i = 0; i < side ** 2; ++i) {
+		out.push(square[i].toString().padStart(4, " "));
+		if ((i + 1) % side === 0) {
+			out.push("\n");
+		}
+	}
+	return out.join("");
+}
